f19968173e87f6b437f092891141c6eeb0a1d120
Mateusz Wiater <mateuszwiater@gmail.com>
2016-10-27 12:00:53 -0400

    Finished the main screen mock.

--- src/main/java/edu/oswego/csc420/schedulegenerator/SectionInformationPanel.java
+++ src/main/java/edu/oswego/csc420/schedulegenerator/SectionInformationPanel.java
@@ -2,13 +2,28 @@ package edu.oswego.csc420.schedulegenerator;
 
 import net.miginfocom.swing.MigLayout;
 
-import javax.swing.JPanel;
-import java.awt.Color;
+import javax.swing.*;
+import java.awt.*;
 
 public class SectionInformationPanel extends JPanel {
 
     SectionInformationPanel() {
         setBackground(Color.WHITE);
-        setLayout(new MigLayout());
+        setLayout(new MigLayout("","[grow,fill]","[][][][grow,fill][]"));
+        final JLabel l = new JLabel("Section Information", SwingConstants.CENTER);
+        add(l, "wrap, span 3");
+        add(new JLabel("Section #"));
+        add(new JLabel("CRN"));
+        add(new JLabel("Teacher"),"wrap");
+        add(new JTextField());
+        add(new JTextField());
+        add(new JTextField(),"wrap");
+        Object rowData[][] = { { "MWF", "08:00AM - 09:00AM" },
+                { "T", "10:00AM - 11:00AM" }};
+        Object columnNames[] = { "Days", "Time" };
+        JTable table = new JTable(rowData, columnNames);
+
+        add(new JScrollPane(table), "wrap, span 3");
+        add(new JButton("New Meeting Time"), "span 3");
     }
 }
