2a5b4f34e89ab44f91a7829f565b8a0d8f7d6ddc
Mateusz Wiater <mateuszwiater@gmail.com>
2016-12-07 17:06:32 -0500

    Minor syntactic changes.

--- src/main/java/edu/oswego/csc420/schedulegenerator/GUI.java
+++ src/main/java/edu/oswego/csc420/schedulegenerator/GUI.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 
 public class GUI extends JFrame {
 
-    Generator gen = new Generator();
+    private Generator gen = new Generator();
 
     GUI(String windowTitle) {
         setTitle(windowTitle);
@@ -17,12 +17,12 @@ public class GUI extends JFrame {
         JFrame frame = new GUI("Course Schedule Generator");
         // JFrame frame = new GeneratedScheduleFrame();
         frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(890,650);
         frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> createGui());
+        javax.swing.SwingUtilities.invokeLater(GUI::createGui);
     }
 }
